4930e1ac811875f3f0b0b9074f2c8f873a805b14
smiley-sachdeva <smiley.smiley1991@gmail.com>
2024-04-04 13:12:32 +0400

    Assign next course to talent automatically when current course status has been changed to completed

--- app/models/course_talent_enrollment.rb
+++ app/models/course_talent_enrollment.rb
@@ -1,5 +1,5 @@
 class CourseTalentEnrollment < ApplicationRecord
-   belongs_to :course
+    belongs_to :course
     belongs_to :talent
 
     belongs_to :talent_learning_path_enrollment
@@ -16,6 +16,11 @@ class CourseTalentEnrollment < ApplicationRecord
 
     def assign_next_course
         #Todo after learning path association
+        current_learning_path = talent.talent_learning_path_enrollments.where(status: "In progress").last&.learning_path
+        curent_sequence = current_learning_path.course_learning_paths.where(course_id: self.course_id).last.sequence
+
+        next_course = current_learning_path.course_learning_paths.where(sequence: curent_sequence + 1).last.course_id
+        talent.course_talent_enrollments.create!(course_id: next_course, status: "In progress", talent_learning_path_enrollment_id: talent_learning_path_enrollment.id)
     end
 
 end

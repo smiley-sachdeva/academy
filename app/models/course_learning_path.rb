d4707fe3c7e554aa5cf1282bb8f7912f4cf38b15
smiley-sachdeva <smiley.smiley1991@gmail.com>
2024-04-17 17:39:06 +0400

    1. Refactored talent learning path 2. Refactored controller

--- app/models/course_learning_path.rb
+++ app/models/course_learning_path.rb
@@ -2,5 +2,10 @@ class CourseLearningPath < ApplicationRecord
     belongs_to :course
     belongs_to :learning_path
 
+    # Validations
     validates :sequence, presence: true
+
+    #scopes
+    # Scope to find sequenced course
+    scope :have_sequence, ->(sequence) { find_by(sequence: sequence) }
 end
